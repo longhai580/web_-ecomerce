b9637ec8881661e15b0adccfb07e73101b43392b
LongHai <nguyenhai3266@gmail.com>
2023-10-25 18:40:24 +0700

    first commit

---
+++ sever/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.mgpis1o.mongodb.net',
+    DB_USER: 'sp_lhai',
+    DB_PASS: '123456A',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'caigicungduoc',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'shoppingonline', // gmail service
+    EMAIL_PASS: 'uxvtzgxzprwxotsf'
+  };
+  module.exports = MyConstants;
